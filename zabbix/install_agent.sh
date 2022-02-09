9013dd7cafedc754cf48d1520878ae0dfbfc22aa
Maks0123 <VadimCorporateUkr@gmail.com>
2022-02-09 22:52:57 +0200

    Update install_agent.sh

--- zabbix/install_agent.sh
+++ zabbix/install_agent.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
 
-if [ -s ./setting.cfg ]; then
-   . ./setting.cfg
+. ./settings.cfg
+
+if [ -s ./settings.cfg ]; then
+   . ./settings.cfg
 else
    echo There is not setting.cfg
 fi
-[ -z "$zabbix_server" ] && zabbix_server= some server
+[ -z "$zabbix_server" ] && zabbix_server=10.142.0.2
 [ -z "$listen_port" ] && listen_port=10050
 
 mkdir -p /sybase/src
 rm -rf /sybase/src/zabbix-$version
 
 cd /sybase/src
-if [ ! -s /sybase/src/zabbix-$version.tar.gz
+if [ ! -s /sybase/src/zabbix-$version.tar.gz ]
 then
-    curl -O https://www.zabbix.com/documentation/$version/manual/concepts/agent
+    wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1EzjshcM0JwX14uRetpTb5JrkFT6k0l97' -O zabbix->
 fi
 
-tar -xzvf /sybase/src/zabbix-$version.tar.gz -C  /sybase/src
-cd /sybase/src
+tar -zxvf zabbix-$version.tar.gz 
+cd /sybase/src/zabbix-$version
+ ./configure --prefix=/sybase/zabbix --enable-agent --disable-dependency-tracking 
+make install
 
+/zabbix/etc/zabbix_agentd.conf
+
+# This is a configuration file for Zabbix agent daemon (Unix)
+# To get more information about Zabbix, visit http://www.zabbix.com
+
+############ GENERAL PARAMETERS #################
+
+### Option: PidFile
+
+PidFile=/sybase/zabbix/zabbix_agentd.pid
+LogFile=/sybase/zabbix/zabbix_agentd.log
+Server=10.142.0.2
+ServerActive=10.142.0.2
+
+Hostname=mysql
+
+
+
+
+
+# apt install libpcre3-dev -y 
+# apt install gcc -y 
+# apt install build-essential
