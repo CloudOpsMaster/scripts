48cf060362fa2aa065d1d7bcf7287b81840d5bde
Maks0123 <vadimcorporateukr@gmail.com>
2022-02-10 21:24:45 +0200

    zabbix agent scripts

--- zabbix/install_agent.sh
+++ zabbix/install_agent.sh
@@ -1,49 +1,84 @@
 #!/usr/bin/env bash
 
-. ./settings.cfg
+confFile=./zabbix_agentd.conf
 
-if [ -s ./settings.cfg ]; then
-   . ./settings.cfg
-else
-   echo There is not setting.cfg
-fi
-[ -z "$zabbix_server" ] && zabbix_server=10.142.0.2
-[ -z "$listen_port" ] && listen_port=10050
+create_directories(){
+   mkdir -p /sybase/src
+   mkdir -p /sybase/zabbix/logs
+   mkdir -p /sybase/zabbix/pid
+}
 
-mkdir -p /sybase/src
-rm -rf /sybase/src/zabbix-$version
+check_settings(){
+  if [ -s ./settings.cfg ]; then
+     . ./settings.cfg
+  else
+   echo "There is not setting.cfg"
+   exit 1
+  fi
+}
 
-cd /sybase/src
-if [ ! -s /sybase/src/zabbix-$version.tar.gz ]
-then
-    wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1EzjshcM0JwX14uRetpTb5JrkFT6k0l97' -O zabbix->
-fi
+remove_old_zabbix(){
+   rm -rf $zabbix_home/$version
+   rm $zabbix_home/$version.tar.gz
+}
 
-tar -zxvf zabbix-$version.tar.gz 
-cd /sybase/src/zabbix-$version
- ./configure --prefix=/sybase/zabbix --enable-agent --disable-dependency-tracking 
-make install
+download_zabbix(){
+    cd $zabbix_home
+    if [ ! -s $zabbix_home/$version.tar.gz ];
+    then
+        curl -O $url
+    fi
+}
 
-/zabbix/etc/zabbix_agentd.conf
+extract_zabbix(){
+   if [ -f $zabbix_home/$version.tar.gz ]; 
+   then
+       cd $zabbix_home &&  tar -zxvf $version.tar.gz  -C $zabbix
+   else
+       echo "There have no $version.tar.gz in $zabbix_home"
+       exit 1
+   fi
+         
+}
 
-# This is a configuration file for Zabbix agent daemon (Unix)
-# To get more information about Zabbix, visit http://www.zabbix.com
-
-############ GENERAL PARAMETERS #################
-
-### Option: PidFile
-
-PidFile=/sybase/zabbix/zabbix_agentd.pid
-LogFile=/sybase/zabbix/zabbix_agentd.log
-Server=10.142.0.2
-ServerActive=10.142.0.2
-
-Hostname=mysql
+configure_host_name(){
+     [ -z "$zabbix_server" ] && zabbix_server=10.142.0.0
+     [ -z "$listen_port" ] && listen_port=10050
+     [ -z "$hostname" ] && hostname=`cat /proc/sys/kernel/hostname` 
+      # echo "$hostname"
+      sed -ie '/Hostname *=/ s|=.*$|='$(echo "$hostname")'|' confFile
+      sed -ie '/PidFile *=/ s|=.*$|='$(echo "$PidFile")'|' confFile
+      sed -ie '/LogFile *=/ s|=.*$|='$(echo "$LogFile")'|' confFile
+      sed -ie '/ServerActive *=/ s|=.*$|='$(echo "$zabbix_server")'|' confFile
+      sed -ie '/Server *=/ s|=.*$|='$(echo "$zabbix_server")'|' confFile
+}
 
 
+copy_zabbix_conf(){
+   if [ -f "$zabbixConfigRemove" ];
+   then
+        echo "File $zabbixConfigRemove exist and then remove"
+        rm $zabbixConfigRemove/zabbix_agentd.conf
+   else 
+        echo "copy zabbix_agentd.conf to zabiix/conf"
+        cp $copy_from/zabbix_agentd.conf $copy_to
+   fi
+}
 
 
+ create_directories
+ check_settings
+ remove_old_zabbix
+ download_zabbix
+ extract_zabbix
+ configure_host_name
+ copy_zabbix_conf
+
+
+
+echo "Success!"
 
 # apt install libpcre3-dev -y 
 # apt install gcc -y 
 # apt install build-essential
+
