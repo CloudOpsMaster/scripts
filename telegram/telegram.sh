061da59f340b60054da7d377f3f4112a7463e77b
Maks0123 <vadimcorporateukr@gmail.com>
2022-03-03 00:21:46 +0200

    terraform

--- telegram/telegram.sh
+++ telegram/telegram.sh
@@ -9,3 +9,8 @@ message="$4"
 
 curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":'$chatId',"text":"'"$subj"'\n'"$message"'"}' "https://api.telegram.org/bot"$token"/sendMessage"
 
+# cp /sybase/scripts/telegram/telegram.sh /sybase/zabbix/share/zabbix/alertscripts
+# chmod +x telegram.sh
+# {ALERT.SENDTO}
+# {ALERT.SUBJECT}
+# {ALERT.MESSAGE}
