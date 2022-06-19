de1cdda2a72ed0199a3767c8699bbb0b967bc5f4
Maks0123 <vadimcorporateukr@gmail.com>
2022-06-19 11:09:23 +0300

    samba cifs

---
+++ samba/cifs-utils.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+ip=$1
+
+if [ -s ./settings.cfg ];
+then 
+    . ./settings.cfg
+else 
+    echo "There no setting.cfg file"
+    exit 1
+fi 
+
+create_direcories(){
+    mkdir -p $src/$shara 
+    mkdir -p $src/$shara/$test_dir 
+
+    if [ ! -d "$src/$shara" ];
+    then 
+        echo "There is no $src/$shara directory in tmp "
+        exit 1
+    fi    
+}
+create_direcories
+
+install_cifs_utils(){
+    apt install cifs-utils -y
+}
+install_cifs_utils
+
+mount(){
+    mount -t cifs //$ip$src $src
+}
+mount
